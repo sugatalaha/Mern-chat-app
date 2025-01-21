777284212c8a5a4005297aab9f5e4b4da2fcd2f1
SUGATA LAHA <sugatalaha@gmail.com>
2025-01-21 18:05:16 +0530

    make ready for deploy

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import { createServer } from "http";
 import express from "express";
 import { Server } from "socket.io";
 
-const PORT = 3000;
+const PORT =process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
